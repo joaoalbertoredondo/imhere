ec79e32338ef8e7ce7f966ab4989faead7f7e9ec
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2025-04-01 11:50:16 -0300

    feat: start of the app

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,26 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native"
+import { styles } from "./styles"
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar!")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor={"#6b6b6b"}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
